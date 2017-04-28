8e091fb270fc7bbae985560d1c30c926cb942fe0
Nugzar Chkhaidze <nchkhaidze@wpi.edu>
2017-04-28 18:18:11 -0400

    Update TSP.py

--- Projects/Project 2/TSP.py
+++ Projects/Project 2/TSP.py
@@ -1,9 +1,10 @@
 # time efficiency O(n!)
 # space efficiency O(n^2)
 fix = [0 ,0 ,0 ,0 ,0]  # distance
-
-def TSP(vertex,dist):
+TSP_vertices = set()
+def TSP(vertex,dist,vertices):
     fix[vertex] = 1
+    vertices.add(vertex)
     
     unused = 0
     for i in range(1,n+1):
@@ -11,18 +12,21 @@ def TSP(vertex,dist):
 
         
     if unused == 0:
-        return dist
+        return dist,vertices
 
     mi=999999999 # infinity
 
     for i in range(1,n+1):
         if i != vertex and fix[i]==0 and weight[vertex][i]!=-1:
-            mi = min ( mi , TSP( i , dist + weight[vertex][i]) )
+            new_dist, new_set = TSP( i , dist + weight[vertex][i] , vertices)
+            if mi>new_dist:
+                mi = new_dist
+                vertices = new_set
 
     fix[vertex]= 0
 
 
-    return mi
+    return mi,vertices
 
 n = 4
 
@@ -31,6 +35,6 @@ weight = [[-1, -1, -1, -1, -1],
           [-1, 1, -1, 4, 2],
           [-1, 5, 4, -1, 1],
           [-1, 3, 2, 1, -1],]
-
-print(TSP(1,0))
+vertex_set = set()
+print(TSP(1,0,vertex_set))
 #assert TSP(1,0) == 6
