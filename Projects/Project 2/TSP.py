e6454045d59f5ad70e3ce5413c1f5ca5ce671160
Nugzar Chkhaidze <nchkhaidze@wpi.edu>
2017-04-28 18:47:21 -0400

    Update TSP.py

--- Projects/Project 2/TSP.py
+++ Projects/Project 2/TSP.py
@@ -1,40 +1,49 @@
 # time efficiency O(n!)
 # space efficiency O(n^2)
 fix = [0 ,0 ,0 ,0 ,0]  # distance
-TSP_vertices = set()
-def TSP(vertex,dist,vertices):
+TSP_vertices = []
+def TSP(vertex,dist,path=None):
+    if path is None:
+        path = [vertex]
     fix[vertex] = 1
-    vertices.add(vertex)
+
+
     
     unused = 0
     for i in range(1,n+1):
         unused += 1 - fix[i]
+ #   print(vertex,dist,path,unused)
+
+    return_vertices = []
 
         
     if unused == 0:
-        return dist,vertices
-
+        if weight[vertex][1]==-1:
+            return 999999999,[]
+        
+        fix[vertex] = 0
+        return dist+weight[vertex][1],path+[1]
+    
     mi=999999999 # infinity
 
     for i in range(1,n+1):
         if i != vertex and fix[i]==0 and weight[vertex][i]!=-1:
-            new_dist, new_set = TSP( i , dist + weight[vertex][i] , vertices)
+            new_dist, new_list = TSP( i , dist + weight[vertex][i] , path + [i])
             if mi>new_dist:
                 mi = new_dist
-                vertices = new_set
-
+                return_vertices = new_list
+    
     fix[vertex]= 0
 
-
-    return mi,vertices
+    return mi,return_vertices
 
 n = 4
 
 weight = [[-1, -1, -1, -1, -1],
           [-1, -1, 1, 5, 3],
           [-1, 1, -1, 4, 2],
-          [-1, 5, 4, -1, 1],
-          [-1, 3, 2, 1, -1],]
-vertex_set = set()
-print(TSP(1,0,vertex_set))
+          [-1, 5, 4, -1, 10000000],
+          [-1, 3, 2, 10000000, -1],]
+
+print(TSP(1,0))
 #assert TSP(1,0) == 6
